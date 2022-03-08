d57bb72801d971cba7fe07ba799aed7cd5d6be4b
citixenken <kmuyesu@gmail.com>
2022-03-08 08:22:45 +0300

    first commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Ken Muyesu on 08/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
